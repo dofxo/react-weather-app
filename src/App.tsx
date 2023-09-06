45991fbe0d6464c796737104f80e8e897a7d24fb
dofxo <kargat504@gmail.com>
2023-09-06 20:42:40 +0330

    refactor(App.tsx): change the fetching data/loading/error messages from states to reactQuery

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,37 @@ import { API_KEY } from "./data/apiKey";
 import CitySearch from "./components/CitySearch";
 import MoonLoader from "react-spinners/ClipLoader";
 import WeatherDetails from "./components/WeatherDetails";
-import { weatherDetailsType } from "./types/types";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 // App component
 const App = () => {
 	// State variables
-	const [weatherInfo, setWeatherDetails] = useState<weatherDetailsType>();
-	const [error, setError] = useState("");
-	const [isLoading, setLoading] = useState<boolean>(false);
+	const [cityName, setCityName] = useState("");
+
+	const {
+		data: weatherInfo,
+		isError,
+		isLoading,
+	} = useQuery({
+		queryKey: ["city", cityName],
+		queryFn: () => getCityWeatherDetails(cityName),
+	});
 
 	// Function to get city weather details
 	const getCityWeatherDetails = async (city: string) => {
 		try {
-			setLoading(true);
+			if (!city) return;
 
+			setCityName(city);
 			// Fetching weather data from the API
 			const { data: resp } = await axios.get(
 				`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
 			);
 
-			// Processing the response
-			setWeatherDetails(resp);
-			setError("");
-		} catch (err) {
-			console.error(err);
-			setError("No City Found!");
-		} finally {
-			setLoading(false);
+			return resp;
+		} catch (error) {
+			console.log(error);
 		}
 	};
 
@@ -41,19 +44,25 @@ const App = () => {
 			<CitySearch getCityWeatherDetails={getCityWeatherDetails} />
 
 			{/* Info container */}
-			<div className="w-full h-20 text-white mt-5 flex flex-col items-center">
-				{/* Conditional rendering based on loading and error states */}
-				{isLoading ? (
-					// Displaying a loading spinner if data is being fetched
-					<MoonLoader color="#67E8F9" size={50} />
-				) : !error ? (
-					// Rendering the WeatherDetails component if there are no errors
-					<WeatherDetails weatherInfo={weatherInfo}></WeatherDetails>
-				) : (
-					// Displaying the error message if there is an error
-					<p className="text-red-500">{error}</p>
-				)}
-			</div>
+			{cityName ? (
+				<div className="w-full h-20 text-white mt-5 flex flex-col items-center">
+					{/* Conditional rendering based on loading and error states */}
+					{isLoading ? (
+						// Displaying a loading spinner if data is being fetched
+						<MoonLoader color="#67E8F9" size={50} />
+					) : isError ? (
+						// Displaying the error message if there is an error
+						<p className="text-red-500">No City Found!</p>
+					) : (
+						// Rendering the WeatherDetails component if there are no errors
+						<WeatherDetails
+							weatherInfo={weatherInfo}
+						></WeatherDetails>
+					)}
+				</div>
+			) : (
+				""
+			)}
 		</>
 	);
 };
