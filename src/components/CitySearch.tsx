ab84594855b0bf43c44121cb96fb24587eb5a2ac
dofxo <kargat504@gmail.com>
2023-09-06 15:43:11 +0330

    refactor(citySeach): change the search system with a form submit

--- src/components/CitySearch.tsx
+++ src/components/CitySearch.tsx
@@ -4,42 +4,46 @@ import { WeatherProps } from "../types/types";
 
 // CitySearch component
 const CitySearch = ({ getCityWeatherDetails }: WeatherProps) => {
-  // State variables
-  const [inputValue, setInputValue] = useState("");
-  const inputRef = useRef<HTMLInputElement>(null);
+	// State variables
+	const [inputValue, setInputValue] = useState("");
+	const inputRef = useRef<HTMLInputElement>(null);
 
-  // Function to handle input value change
-  const handleInputValueChange = (value: string) => {
-    setInputValue(value);
-  };
+	// Function to handle input value change
+	const handleInputValueChange = (value: string) => {
+		setInputValue(value);
+	};
 
-  return (
-    <div className="flex flex-col gap-y-5 max-w-[200px]">
-      {/* Input field */}
-      <input
-        ref={inputRef}
-        type="text"
-        placeholder="select your city"
-        className="outline-none p-1.5 rounded bg-cyan-300 max-w-[200px] placeholder:text-[14px]"
-        onChange={() => {
-          if (inputRef.current) {
-            handleInputValueChange(inputRef.current.value);
-          }
-        }}
-      />
-      {/* Search button */}
-      <button
-        type="submit"
-        disabled={!inputValue ? true : false}
-        onClick={() => {
-          if (inputValue) getCityWeatherDetails(inputValue);
-        }}
-        className="rounded p-1 bg-cyan-400 active:scale-90 transition disabled:bg-slate-500 disabled:cursor-not-allowed cursor-pointer"
-      >
-        Search
-      </button>
-    </div>
-  );
+	return (
+		<form
+			className="flex flex-col gap-y-5 max-w-[200px]"
+			onSubmit={(e) => {
+				e.preventDefault();
+			}}
+		>
+			{/* Input field */}
+			<input
+				ref={inputRef}
+				type="text"
+				placeholder="select your city"
+				className="outline-none p-1.5 rounded bg-cyan-300 max-w-[200px] placeholder:text-[14px]"
+				onChange={() => {
+					if (inputRef.current) {
+						handleInputValueChange(inputRef.current.value);
+					}
+				}}
+			/>
+			{/* Search button */}
+			<input
+				type="submit"
+				value="Search"
+				disabled={!inputValue ? true : false}
+				onClick={() => {
+					if (inputValue) getCityWeatherDetails(inputValue);
+				}}
+				className="rounded p-1 bg-cyan-400 active:scale-90 transition disabled:bg-slate-500 disabled:cursor-not-allowed cursor-pointer"
+			/>
+		</form>
+	);
 };
 
 export default CitySearch;
