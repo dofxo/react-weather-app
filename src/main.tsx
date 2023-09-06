b4e1bad9db1570dc814e8e3864a1d77f91469614
dofxo <kargat504@gmail.com>
2023-09-06 20:42:09 +0330

    feat(reactQuery): add and config reactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.scss";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const useQuery = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<QueryClientProvider client={useQuery}>
+			<App />
+		</QueryClientProvider>
+	</React.StrictMode>
+);
