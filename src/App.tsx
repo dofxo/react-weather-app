1b73006601a153886475e1bb00cc7891188927cd
dofxo <kargat504@gmail.com>
2023-09-06 15:14:30 +0330

    document(components): add some comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Importing necessary dependencies and components
 import { useState } from "react";
 import { API_KEY } from "./data/apiKey";
 import CitySearch from "./components/CitySearch";
@@ -5,39 +6,49 @@ import MoonLoader from "react-spinners/ClipLoader";
 import WeatherDetails from "./components/WeatherDetails";
 import { weatherDetailsType } from "./types/types";
 
+// App component
 const App = () => {
+	// State variables
 	const [weatherInfo, setWeatherDetails] = useState<weatherDetailsType>();
 	const [error, setError] = useState("");
 	const [isLoading, setLoading] = useState<boolean>(false);
 
-	// getting city weather details
+	// Function to get city weather details
 	const getCityWeatherDetails = async (city: string) => {
 		setLoading(true);
 
+		// Fetching weather data from the API
 		const resp = await fetch(
 			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
 		);
 		const jsonedResp = await resp.json();
 
+		// Processing the response
 		if (jsonedResp.cod === 200) {
 			setWeatherDetails(jsonedResp);
 			setError("");
 		}
 		if (jsonedResp.cod === "404") setError("No City Found!");
+
 		setLoading(false);
 	};
 
 	return (
 		<>
+			{/* Rendering the CitySearch component */}
 			<CitySearch getCityWeatherDetails={getCityWeatherDetails} />
 
-			{/* info container */}
-			<div className=" w-full h-20 text-white mt-5 flex flex-col items-center">
+			{/* Info container */}
+			<div className="w-full h-20 text-white mt-5 flex flex-col items-center">
+				{/* Conditional rendering based on loading and error states */}
 				{isLoading ? (
+					// Displaying a loading spinner if data is being fetched
 					<MoonLoader color="#67E8F9" size={50} />
 				) : !error ? (
+					// Rendering the WeatherDetails component if there are no errors
 					<WeatherDetails weatherInfo={weatherInfo}></WeatherDetails>
 				) : (
+					// Displaying the error message if there is an error
 					<p className="text-red-500">{error}</p>
 				)}
 			</div>
--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -1,39 +1,45 @@
+// Importing necessary type
 import { weatherDetailsType } from "../types/types";
 
+// WeatherDetails component
 const WeatherDetails = ({
-	weatherInfo,
+  weatherInfo,
 }: {
-	weatherInfo?: weatherDetailsType;
+  weatherInfo?: weatherDetailsType;
 }) => {
-	if (!weatherInfo) return <div></div>;
+  // Checking if weatherInfo is available
+  if (!weatherInfo) return <div></div>;
 
-	return (
-		<div className="flex flex-col w-full">
-			<span className="title text-cyan-600 text-xl flex justify-between items-center">
-				{weatherInfo.name}
-				<img
-					src={`https://openweathermap.org/img/wn/${weatherInfo.weather[0]?.icon}@2x.png`}
-					alt="weather"
-				/>
-			</span>
-			<div className="details flex items-center justify-between">
-				<p className="text-yellow-200 text-xl">
-					{weatherInfo.main.temp ? (
-						<span className="relative">
-							{Math.floor(weatherInfo.main.temp)}{" "}
-							<span className="absolute text-sm">°C</span>
-						</span>
-					) : (
-						""
-					)}
-				</p>
-				<div className="flex flex-col text-sm">
-					<span>Humidity: {weatherInfo.main.humidity}%</span>
-					<span>Wind: {Math.floor(weatherInfo.wind.speed)} km/h</span>
-				</div>
-			</div>
-		</div>
-	);
+  // Rendering weather details
+  return (
+    <div className="flex flex-col w-full">
+      {/* Rendering city name and weather icon */}
+      <span className="title text-cyan-600 text-xl flex justify-between items-center">
+        {weatherInfo.name}
+        <img
+          src={`https://openweathermap.org/img/wn/${weatherInfo.weather[0]?.icon}@2x.png`}
+          alt="weather"
+        />
+      </span>
+      {/* Rendering temperature, humidity, and wind details */}
+      <div className="details flex items-center justify-between">
+        <p className="text-yellow-200 text-xl">
+          {weatherInfo.main.temp ? (
+            <span className="relative">
+              {Math.floor(weatherInfo.main.temp)}{" "}
+              <span className="absolute text-sm">°C</span>
+            </span>
+          ) : (
+            ""
+          )}
+        </p>
+        <div className="flex flex-col text-sm">
+          <span>Humidity: {weatherInfo.main.humidity}%</span>
+          <span>Wind: {Math.floor(weatherInfo.wind.speed)} km/h</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WeatherDetails;
